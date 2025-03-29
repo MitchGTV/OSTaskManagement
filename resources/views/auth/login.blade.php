1a64a7cdd8642e4be8a33384b21896272b1a6e07
Mitchell Butterfield <mitchellcann98@gmail.com>
2025-03-29 19:05:19 +0000

    Clear cache and re-add all files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,7 @@
                     <div class="col-sm-10 col-md-8 col-lg-5 col-xl-5 col-xxl-3">
                         <div class="text-center mb-7">
                             <h3 class="text-1000">Sign In</h3>
-                            <p class="text-700">Get access to your account</p>
-                        </div><button class="btn btn-phoenix-secondary w-100 mb-3"><span class="fab fa-google text-danger me-2 fs--1"></span>Sign in with google</button><button class="btn btn-phoenix-secondary w-100"><span class="fab fa-facebook text-primary me-2 fs--1"></span>Sign in with facebook</button>
-                        <div class="position-relative">
-                            <hr class="bg-200 mt-5 mb-4" />
-                            <div class="divider-content-center">or use email</div>
+                            <p class="text-700">Sign in to your account</p>
                         </div>
                         <div class="mb-3 text-start"><label class="form-label" for="email">Email address</label>
                             <div class="form-icon-container"><input class="form-control form-icon-input" id="email" type="email" placeholder="name@example.com" /><span class="fas fa-user text-900 fs--1 form-icon"></span></div>
