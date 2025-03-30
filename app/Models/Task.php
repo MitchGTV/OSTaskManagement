1ea4ffabbac7447976036ee52cdaba2d2f98e4a9
Mitchell Butterfield <mitchellcann98@gmail.com>
2025-03-30 22:27:33 +0100

    add migration and start backend work on tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $table = 'tasks';
+
+    protected $fillable = ['id', 'user_id', 'description', 'priority'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
